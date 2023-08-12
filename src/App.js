25cf7d218323c2e443d2daafb6412586ba4b27c4
Sadebeauty <124639292+Sadebeauty@users.noreply.github.com>
2023-08-12 03:34:42 +0100

    Modified the weatherApp  html and included CSS file

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
 /** @format */
-
+import WeatherApp from "./WeatherApp";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        {" "}
-        <ul className=" cityHeader d-flex">
-          <li>Lagos</li>
-          <li> Ibadan</li>
-          <li>Abuja</li>
-          <li> Kano</li>
-        </ul>{" "}
-        <div className="row">
-          <form>
-            <input
-              className="col-9"
-              type="search"
-              placeholder="Type a city..."
-            />
-            <input
-              className="col-3 "
-              type="Submit"
-              defaultValue="Search"
-            />
-          </form>
-        </div>
-        <div className=" cityDescription  row">
-          <div className=" col-6">
-            <h1 className="cityName mb-0">Lagos</h1>
-            <br /> <h5 className="date mt-0">Tuesday, 8th August</h5>
-            <br />
-            <h6 className="description mt-0">Cloudy</h6>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Temperature</li> <li>Description</li>
-              <li>Humidity</li>
-              <li>Wind</li>
-            </ul>
-          </div>
-          <div className="row d-flex">
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/10d@2x.png"
-                alt={" "}></img>
-            </div>
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/02d@2x.png"
-                alt={" "}></img>
-            </div>
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/11d@2x.png"
-                alt={" "}></img>
-            </div>
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/10d@2x.png"
-                alt={" "}></img>
-            </div>
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/04d@2x.png"
-                alt={" "}></img>
-            </div>
-            <div className="col-2">
-              <img
-                srcSet=" https://openweathermap.org/img/wn/09d@2x.png"
-                alt={" "}></img>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <small>
+      <WeatherApp />
+      <footer>
         <a href="https://github.com/Sadebeauty/weather-app">
           Open-source code{" "}
         </a>
         by Folasade Adesope
-      </small>
+      </footer>
     </div>
   );
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,70 +1,49 @@
 /** @format */
 
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import "./WeatherApp.css";
 
 export default function WeatherApp() {
-  let [City, setCity] = useState("");
-  let [Response, setResponse] = useState("");
-  let [weather, setWeather] = useState(null);
+  return (
+    <div className="container">
+      <div className="row">
+        <form>
+          <input
+            className="col-9"
+            type="search"
+            placeholder="Type a city..."
+          />
+          <input
+            className="col-3 "
+            type="Submit"
+            defaultValue="Search"
+          />
+        </form>
+      </div>
+      <h1>Lagos</h1>
+      <ul className="weatherDescription">
+        <li className="date">Tuesday, 8th August</li>
+        <li className="description">Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className=" col-6">
+          <img
+            className=" icon float-left"
+            src=" https://openweathermap.org/img/wn/02d@2x.png"
+            alt={" "}
+          />
+          <span className="weatherTemperature float-left">25</span>
+          <span className="units">°C</span>
+        </div>
 
-  function displayWeather(response) {
-    setResponse(true);
-    setWeather({
-      Temperature: response.data.main.temp,
-      Description: response.data.weather[0].description,
-      Humidity: response.data.main.humidity,
-      Wind: response.data.wind.speed,
-      icon: `Https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
-    });
-  }
-
-  function HandleQuery(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=b4ca63d943f7c96a9ccdd7dada737c1e`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function ChangeCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <div className="row">
-      <form onSubmit={HandleQuery}>
-        <input
-          className="col-9"
-          type="search"
-          placeholder="Type a city..."
-          onChange={ChangeCity}
-        />
-        <input
-          className="col-3"
-          type="Submit"
-          defaultValue="Search"
-        />
-      </form>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:4%</li>
+            <li>Humidity: 91%</li>
+            <li>Wind: 8km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-  if (Response) {
-    return (
-      <div className="Wrapper">
-        {form}
-        <ul className="co-6">
-          <li key="index">Temperature:{Math.round(weather.Temperature)}°C</li>
-          <li>Description:{weather.Description}</li>
-          <li>Humidity:{weather.Humidity}%</li>
-          <li>Wind:{weather.Wind}km/h</li>
-          <li>
-            <img
-              src={weather.icon}
-              alt={" "}
-            />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return form;
-  }
 }
